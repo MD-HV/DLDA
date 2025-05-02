dd764c8d127b6f117cd475c64d63e924338b75bf
MD-HV <mida0022@student.hv.se>
2025-05-02 11:01:59 +0200

    Create User.cs

---
+++ DLDA.API/DLDA.API/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DLDA.API.Models
+{
+    [PrimaryKey(nameof(UserId))]
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
