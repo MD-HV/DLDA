8a313b565e4908cc8f9679f0a00b6268e88b4db2
MD-HV <mida0022@student.hv.se>
2025-05-02 11:15:00 +0200

    models uppdatering

---
+++ DLDA.API/DLDA.API/Models/Assessment.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DLDA.API.Models
+{
+    [PrimaryKey(nameof(AssessmentId))]
+    public class Assessment
+    {
+        public int AssessmentId { get; set; }
+        public string Type { get; set; }
+        public string ScaleType { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsComplete { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
