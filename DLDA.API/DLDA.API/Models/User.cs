3afc2a88379bb9bd6d16e71f34f27605e0d2418e
Victor K <meisterkoff@hotmail.se>
2025-05-02 11:59:24 +0200

    merge conflicts

--- DLDA.API/DLDA.API/Models/User.cs
+++ DLDA.API/DLDA.API/Models/User.cs
@@ -8,19 +8,14 @@ namespace DLDA.API.Models
         //Primarykey för users
         public int UserID { get; set; }
         //Användarnamn för users
-        public string Username { get; set; }
-        //Epostadress för användaren
-        public string? Email { get; set; }
-        //Hash för lösenord
-        public string PasswordHash { get; set; }
-        //Vilken roll användaren har, om det är en patient eller skötare
-        public string Role { get; set; }
-        //När användaren skapades
-        public int UserId { get; set; }
         public string Username { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        //Epostadress för användaren
+        public string? Email { get; set; } = string.Empty;
+        //Hash för lösenord
         public string PasswordHash { get; set; } = string.Empty;
+        //Vilken roll användaren har, om det är en patient eller skötare
         public string Role { get; set; } = string.Empty;
+        //När användaren skapades
         public DateTime CreatedAt { get; set; }
 
         public ICollection<Assessment> Assessments { get; set; } = new List<Assessment>();
