15539eccebd820720ffee33d4ab9f5c93fc4021f
Victor K <meisterkoff@hotmail.se>
2025-05-02 11:20:31 +0200

    La till model för AssessmentItem och Question

---
+++ DLDA.API/DLDA.API/Models/AssessmentItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DLDA.API.Models
+{
+    public class AssessmentItem
+    {
+        // Primärnyckel
+        public int ItemID { get; set; }
+
+        // Foreign key för koppling till Assessment
+        public int AssessmentID { get; set; }
+
+        // Foreign key för koppling till Question
+        public int QuestionID { get; set; }
+
+        // Skattningsvärde (0 = inget problem, 4 = mycket stort problem)
+        public int AnswerValue { get; set; }
+
+        // När frågan besvarades
+        public DateTime AnsweredAt { get; set; } = DateTime.UtcNow;
+
+        // Flagga för vidare diskussion
+        public bool Flag { get; set; }
+
+        // Navigationsegenskaper
+        public Assessment Assessment { get; set; }
+        public Question Question { get; set; }
+    }
+}
