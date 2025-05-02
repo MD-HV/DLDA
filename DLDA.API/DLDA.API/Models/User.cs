5843636d38c3b5543e8b79af06288337061405b4
MD-HV <mida0022@student.hv.se>
2025-05-02 11:28:49 +0200

    Update User.cs

--- DLDA.API/DLDA.API/Models/User.cs
+++ DLDA.API/DLDA.API/Models/User.cs
@@ -5,11 +5,17 @@ namespace DLDA.API.Models
     [PrimaryKey(nameof(UserID))]
     public class User
     {
+        //Primarykey för users
         public int UserID { get; set; }
+        //Användarnamn för users
         public string Username { get; set; }
+        //Epostadress för användaren
         public string Email { get; set; }
+        //Hash för lösenord
         public string PasswordHash { get; set; }
+        //Vilken roll användaren har, om det är en patient eller skötare
         public string Role { get; set; }
+        //När användaren skapades
         public DateTime CreatedAt { get; set; }
     }
 }
